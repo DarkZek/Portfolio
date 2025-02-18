13e983a5fd5b81b3e9531b65e0309268bb8a6049
Marshall Scott <masheddarkzek@gmail.com>
2025-02-18 20:19:27 +1300

    feat: Show latest blog post as social preview

--- client/src/consts.ts
+++ client/src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Marshall Scott';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'I\'m a Software Engineer with a passion for computer graphics.';
